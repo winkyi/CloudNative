520baa75b2ac85e49f944ceca2d2e3b0595140c4
winkyi <winkyi@163.com>
2021-10-09 17:36:39 +0800

    使用errgroup管理各个http服务的生命周期

--- httpserver/main.go
+++ httpserver/main.go
@@ -1,21 +1,35 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"github.com/winkyi/CloudNative/httpserver/engine"
+	"golang.org/x/sync/errgroup"
 	"net/http"
 	_ "net/http/pprof"
 )
 
 func main() {
+	g, _ := errgroup.WithContext(context.Background())
 	r_app := engine.New()
 	r_app.GET("/", engine.Index)
-	go r_app.Run(":9999")
+	g.Go(func() error {
+		return r_app.Run(":9999")
+	})
 
 	r_health := engine.New()
 	r_health.GET("/healthz", engine.Healthz)
-	go r_health.Run(":80")
+	g.Go(func() error {
+		return r_health.Run(":80")
+	})
 
 	// pprof
-	go http.ListenAndServe(":8001", http.DefaultServeMux)
-	select {}
+	g.Go(func() error {
+		return http.ListenAndServe(":8001", http.DefaultServeMux)
+	})
+
+	err := g.Wait()
+	if err != nil {
+		fmt.Println(err)
+	}
 }
